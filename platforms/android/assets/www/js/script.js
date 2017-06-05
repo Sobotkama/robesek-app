2d070823990efddd864c16a87653d11249e92b47
Sobotkama <sobotkama@gmail.com>
2017-06-05 20:58:52 +0200

    added settings and stuff

--- platforms/android/assets/www/js/script.js
+++ platforms/android/assets/www/js/script.js
@@ -1,70 +1,85 @@
 //
 // MJPEG
 //
+var ip = "192.168.104.143"
 var mjpeg_img;
 var halted = 0;
 var previous_halted = 99;
 var mjpeg_mode = 0;
 var preview_delay = 0;
+var modal;
+var span;
 
-function reload_img () {
-  if(!halted) mjpeg_img.src = "http://192.168.104.143/cam.jpg?" + new Date().getTime();
+function reload_img() {
+  if (!halted) mjpeg_img.src = "http://" + ip + "/cam.jpg?" + new Date().getTime();
   else setTimeout("reload_img()", 500);
 }
 
-function error_img () {
-  setTimeout("mjpeg_img.src = 'http://192.168.104.143/cam.jpg?time=' + new Date().getTime();", 100);
+function error_img() {
+  mjpeg_img.src = 'img/err.png'
 }
 
-function updatePreview(cycle)
-{
-   if (mjpegmode)
-   {
-      if (cycle !== undefined && cycle == true)
-      {
-         setTimeout("mjpeg_img.src = \"http://192.168.104.143/cam.jpg?time=\" + new Date().getTime();", 1000);
-         return;
-      }
-      
-      if (previous_halted != halted)
-      {
-         if(!halted)
-         {
-	     mjpeg_img.src = "http://192.168.104.143/cam.jpg?time=" + new Date().getTime() ;
-	}
-      }
-	previous_halted = halted;
-   }
-}
+//
+// INTERFACE
+//
+
+//JOYSTICK
 
 function createnipple() {
-    var joystickL = nipplejs.create({
-        zone: document.getElementById('left'),
-        mode: 'dynamic',
-        color: 'red',
-        size: 80
-    });
+  var joystickL = nipplejs.create({
+    zone: document.getElementById('left'),
+    mode: 'dynamic',
+    color: 'red',
+    size: 100
+  });
 }
 
+//SETTINGS
+
+// Get the modal
+
+
+// When the user clicks on <span> (x), close the modal
+function spanc() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+function settwindow() {
+  console.log("settings")
+  modal.style.display = "block";
+}
+
+function savevars() {
+  ip=document.getElementById("ipbox").value;
+}
 
 //
 // Init
 //
-function init(mjpeg, video_fps, divider) {
+function init() {
+  var modal = document.getElementById('modalSettings');
+  var span = document.getElementsByClassName("close")[0];
   createnipple();
   mjpeg = 0;
   video_fps = 25;
   divider = 1
   mjpeg_img = document.getElementById("mjpeg_dest");
-  preview_delay = Math.floor(divider / Math.max(video_fps,1) * 1000000);
+  preview_delay = Math.floor(divider / Math.max(video_fps, 1) * 1000000);
   if (mjpeg) {
     mjpegmode = 1;
-  } else {
-     mjpegmode = 0;
-     mjpeg_img.onload = reload_img;
-     mjpeg_img.onerror = error_img;
-     reload_img();
+  }
+  else {
+    mjpegmode = 0;
+    mjpeg_img.onload = reload_img;
+    mjpeg_img.onerror = error_img;
+    reload_img();
   }
 }
-
-
