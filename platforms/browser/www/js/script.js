62a219fba5317e6de0b7598356dc897e45bb316f
Sobotkama <sobotkama@gmail.com>
2017-06-14 22:39:07 +0200

    bugfixes and more storage functions

--- platforms/browser/www/js/script.js
+++ platforms/browser/www/js/script.js
@@ -9,14 +9,42 @@ var mjpeg_mode = 0;
 var preview_delay = 0;
 var modal;
 var span;
+var storage = window.localStorage;
+var rtext;
+var error = 0;
+var fps = 15
+
+
+function sendData(data) {
+  document.getElementById("debugbox").innerHTML = data
+
+} 
 
 function reload_img() {
-  setTimeout("mjpeg_img.src = 'http://' + ip + '/cam.jpg?' + new Date().getTime();", preview_delay )
+  refreshIntervalId = setInterval(function(){ 
+    if (error == 0) {
+      mjpeg_img.src = 'http://' + ip + '/cam.jpg?' + new Date().getTime();
+    }
+    else {
+    clearInterval(refreshIntervalId);
+    }     
+  }, preview_delay);
+}
+
+
+function unerror() {
+  setTimeout(function(){
+  error = 0
+  reload_img()
+  },100)
 }
 
 function error_img() {
-  mjpeg_img.src = 'img/err.png'
-  setTimeout("reload_img()", 1000)
+  if (mjpeg_img.src != 'img/err.png') {
+    mjpeg_img.src = 'img/err.png'
+  }
+  error = 1
+  setTimeout("unerror()", 5000)
 }
 
 //
@@ -26,17 +54,29 @@ function error_img() {
 //JOYSTICK
 
 function resetcam() {
-  document.getElementById("hors").value="90"
-  document.getElementById("vers").value="110"
+  document.getElementById("hors").value = "90"
+  document.getElementById("vers").value = "110"
+  camhor()
+  camver()
 }
 
 function createnipple() {
-  var joystickL = nipplejs.create({
+  manager = nipplejs.create({
     zone: document.getElementById('left'),
     mode: 'dynamic',
     color: 'red',
     size: 100
   });
+}
+
+function camhor(){
+  var payload = "c:hor" + document.getElementById("hors").value
+  sendData(payload)
+}
+
+function camver(){
+  var payload = "c:ver" + document.getElementById("vers").value
+  sendData(payload)
 }
 
 //SETTINGS
@@ -53,14 +93,41 @@ window.onclick = function (event) {
 
 
 function settwindow() {
-  console.log("settings")
   modal.style.display = "block";
 }
 
 function savevars() {
   ip = document.getElementById("ipbox").value;
-  reload_img()
+  rtext = document.getElementById("rtextbox").value
+  fps = Number(document.getElementById("fps").value)
+  storage.setItem("rtext", rtext);
+  storage.setItem("ip", ip)
+  storage.setItem("fps", fps)
+  preview_delay = Math.floor(divider / Math.max(fps, 1) * 1000);
+  error = 1
+  setTimeout(function(){unerror()},preview_delay)
 }
+
+function loadVars() {
+  ip = storage.getItem("ip")
+  rtext = storage.getItem("rtext")
+  fps = storage.getItem("fps")
+  if (ip != null) {
+    document.getElementById("ipbox").value = ip
+    clearInterval(refreshIntervalId);
+    reload_img()
+  }
+  if (fps != null) {
+    document.getElementById("fps").value = fps
+    preview_delay = Math.floor(divider / Math.max(fps, 1) * 1000)
+    clearInterval(refreshIntervalId);
+    reload_img()
+  }
+  document.getElementById("rtextbox").value = rtext
+}
+
+createnipple()
+
 
 //
 // Init
@@ -68,13 +135,39 @@ function savevars() {
 function init() {
   var modal = document.getElementById('modalSettings');
   var span = document.getElementsByClassName("close")[0];
-  createnipple();
-  video_fps = 15;
-  divider = 1
+  divider = 1;
   mjpeg_img = document.getElementById("mjpeg_dest");
-  preview_delay = Math.floor(divider / Math.max(video_fps, 1) * 1000);
+  preview_delay = Math.floor(divider / Math.max(fps, 1) * 1000);
   mjpegmode = 0;
-  mjpeg_img.onload = reload_img;
   mjpeg_img.onerror = error_img;
   reload_img();
+  loadVars()
 }
+
+
+manager.on('dir:up', function () {
+    console.log("up")
+    sendData("m:for")
+    })
+
+manager.on('dir:down', function () {
+    console.log("down")
+    sendData("m:back")
+    })
+    
+manager.on('dir:left', function () {
+    console.log("left")
+    sendData("m:left")
+    })    
+
+manager.on('dir:right', function () {
+    console.log("right")
+    sendData("m:right")
+    })
+manager.on('end', function () {
+    console.log("release")
+    sendData("m:stop")
+    })
+
+
+
--- www/js/script.js
+++ www/js/script.js
@@ -12,17 +12,31 @@ var span;
 var storage = window.localStorage;
 var rtext;
 var error = 0;
+var fps = 15
+
+
+function sendData(data) {
+  document.getElementById("debugbox").innerHTML = data
+
+} 
 
 function reload_img() {
-  if (error = 0) {
-    setTimeout("mjpeg_img.src = 'http://' + ip + '/cam.jpg?' + new Date().getTime();", preview_delay)
-    reload_img;
-  }
+  refreshIntervalId = setInterval(function(){ 
+    if (error == 0) {
+      mjpeg_img.src = 'http://' + ip + '/cam.jpg?' + new Date().getTime();
+    }
+    else {
+    clearInterval(refreshIntervalId);
+    }     
+  }, preview_delay);
 }
 
+
 function unerror() {
+  setTimeout(function(){
   error = 0
-  reload_img
+  reload_img()
+  },100)
 }
 
 function error_img() {
@@ -42,15 +56,27 @@ function error_img() {
 function resetcam() {
   document.getElementById("hors").value = "90"
   document.getElementById("vers").value = "110"
+  camhor()
+  camver()
 }
 
 function createnipple() {
-  var joystickL = nipplejs.create({
+  manager = nipplejs.create({
     zone: document.getElementById('left'),
     mode: 'dynamic',
     color: 'red',
     size: 100
   });
+}
+
+function camhor(){
+  var payload = "c:hor" + document.getElementById("hors").value
+  sendData(payload)
+}
+
+function camver(){
+  var payload = "c:ver" + document.getElementById("vers").value
+  sendData(payload)
 }
 
 //SETTINGS
@@ -67,18 +93,19 @@ window.onclick = function (event) {
 
 
 function settwindow() {
-  console.log("settings");
   modal.style.display = "block";
 }
 
 function savevars() {
   ip = document.getElementById("ipbox").value;
-  reload_img();
   rtext = document.getElementById("rtextbox").value
   fps = Number(document.getElementById("fps").value)
   storage.setItem("rtext", rtext);
   storage.setItem("ip", ip)
   storage.setItem("fps", fps)
+  preview_delay = Math.floor(divider / Math.max(fps, 1) * 1000);
+  error = 1
+  setTimeout(function(){unerror()},preview_delay)
 }
 
 function loadVars() {
@@ -87,13 +114,20 @@ function loadVars() {
   fps = storage.getItem("fps")
   if (ip != null) {
     document.getElementById("ipbox").value = ip
+    clearInterval(refreshIntervalId);
+    reload_img()
   }
   if (fps != null) {
     document.getElementById("fps").value = fps
-    preview_delay = Math.floor(divider / Math.max(video_fps, 1) * 1000)
+    preview_delay = Math.floor(divider / Math.max(fps, 1) * 1000)
+    clearInterval(refreshIntervalId);
+    reload_img()
   }
   document.getElementById("rtextbox").value = rtext
 }
+
+createnipple()
+
 
 //
 // Init
@@ -101,13 +135,39 @@ function loadVars() {
 function init() {
   var modal = document.getElementById('modalSettings');
   var span = document.getElementsByClassName("close")[0];
-  //createnipple();
-  video_fps = 15;
   divider = 1;
   mjpeg_img = document.getElementById("mjpeg_dest");
-  preview_delay = Math.floor(divider / Math.max(video_fps, 1) * 1000);
+  preview_delay = Math.floor(divider / Math.max(fps, 1) * 1000);
   mjpegmode = 0;
   mjpeg_img.onerror = error_img;
   reload_img();
   loadVars()
 }
+
+
+manager.on('dir:up', function () {
+    console.log("up")
+    sendData("m:for")
+    })
+
+manager.on('dir:down', function () {
+    console.log("down")
+    sendData("m:back")
+    })
+    
+manager.on('dir:left', function () {
+    console.log("left")
+    sendData("m:left")
+    })    
+
+manager.on('dir:right', function () {
+    console.log("right")
+    sendData("m:right")
+    })
+manager.on('end', function () {
+    console.log("release")
+    sendData("m:stop")
+    })
+
+
+
